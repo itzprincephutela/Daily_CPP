79793e9b4bd3b1ac221fdd37d959f8f98279ea6f
Prince <122357150+itzprincephutela@users.noreply.github.com>
2023-08-28 13:48:33 +0530

    Add files via upload

---
+++ Data type and type Modifiers/SizeofInt.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+ int main ()
+ {
+    int a;
+    a = 12;
+    cout<< "Size of int "<<sizeof(a)<<endl;
+    float b ;
+    b = 2.0;
+    cout << "Size of float"<<sizeof(b)<<endl;
+    char c;
+    c='c';
+    cout << "Size of Char"<<sizeof(c)<<endl;
+
+ }

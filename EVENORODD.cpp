0903d3ab7a1423fdb61f891407bc443be6f43651
Prince <122357150+itzprincephutela@users.noreply.github.com>
2023-08-29 14:02:12 +0530

    Add files via upload

---
+++ EVENORODD.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main (){
+    int N;
+    cout<<"Enter the number\n";
+    cin>>N;
+    if(N%2==0){
+        cout<<"EVEN NUMBER";
+    }
+    else{
+        cout<<"ODD NUMBER";
+    }
+}

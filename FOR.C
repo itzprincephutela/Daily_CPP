c3ee219963c8bd2b99e65a55be65f0e86c815f09
Prince <122357150+itzprincephutela@users.noreply.github.com>
2023-08-30 20:05:52 +0530

    Add files via upload

---
+++ FOR.C
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+int main 
+(){
+
+    int  n ;
+    cout<<"ENTER THE NUMBER"<<endl;
+    cin>>n;
+    for(int count=0 ; count<=n; count++){
+        cout<<"HELLO WORLD COUNT:"<<count<<endl;
+    }
+}

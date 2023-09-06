387f8e0ed205f74f85286ca74960746bba926aa9
Prince <122357150+itzprincephutela@users.noreply.github.com>
2023-09-06 20:05:36 +0530

    Add files via upload

---
+++ FILE.c
@@ -0,0 +1,12 @@
+#include<stdio.h> //include is preprocessor//
+#include<math.h>
+#include<limit.h>
+int main (){
+int x = INT_MAX;
+//int x=__INT_MAX__;
+printf("%d\n",x);//int is using  4 byte = 2*32 bits
+//long is using 8 byte =2*64 bits;
+printf("HELLO WORLD");
+
+
+}

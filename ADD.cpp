52a52a0e62d7302a88666bfab7c60ca6ae375966
Prince <122357150+itzprincephutela@users.noreply.github.com>
2023-10-18 17:28:12 +0530

    Add files via upload

---
+++ ADD.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int add(int num1,int num2){
+    int sum=num1+num2;
+}
+int main (){
+        int a;  
+        int b;
+        cin>>a>>b;
+        cout<<add(a,b)<<endl;
+}

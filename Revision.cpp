fd637ce77d81b3a4f497322149acd97baf9fdbd0
Prince <122357150+itzprincephutela@users.noreply.github.com>
2023-10-31 17:28:21 +0530

    Add files via upload

---
+++ Revision.cpp
@@ -0,0 +1,66 @@
+// transpose
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     int arr[3][3]={1,2,3,4,5,6,7,8,9};
+//     int arr2[3][3];
+//     for (int i=0;i<3;i++){
+//         for(int j=0;j<3;j++){
+//             arr2[i][j]=arr[j][i];
+//         }    
+// }
+//     for(int i=0;i<3;i++){
+//         for(int j=0;j<3;j++){
+//             cout<<arr2[i][j]<<" ";  
+//         }    
+//         cout<<endl;
+// }
+// }
+
+//90%
+// #include<iostream>
+// using namespace std;
+// int main (){
+//     int arr[3][3]={1,2,3,4,5,6,7,8,9};
+//     int arr2[3][3];
+//     for (int i =0 ; i < 3 ; i++){
+//         for(int j=0 ;j< 3 ; j++){
+//              arr2[i][j]==arr[j][i];
+//         }
+//     }
+//     for (int i =0 ; i < 3 ; i++){
+//         for(int j=2 ;j>=0 ; j--){
+//              cout<<arr[i][j]<<" ";
+//         }cout<<endl;
+//     }
+// }
+
+//WAVE PRINT -->
+// #include<iostream>
+// using namespace std;
+// int main (){
+// int arr[3][3]={1,2,3,4,5,6,7,8,9};
+// for(int i =0; i<3 ; i++){
+//     if(i%2==0){
+//         for(int j=0 ; j<3 ;j++){
+//             cout<<arr[i][j]<<" ";
+//         }
+//     }
+//     else {
+//         for(int j=2 ; j>=0 ; j--){
+//             cout<<arr[i][j]<<" ";
+//         }
+//     }
+//     cout<<endl;
+
+// }
+
+// }
+
+
+// WAVE PRINT 
+#include<iostream>
+using namespace std;
+int main (){
+    
+} 

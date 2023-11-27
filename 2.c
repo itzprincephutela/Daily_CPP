ddbf666511356a53e4164a6b5ba22f10ec9aaf85
Prince <122357150+itzprincephutela@users.noreply.github.com>
2023-11-27 12:11:09 +0530

    Add files via upload

--- 2.c
+++ 2.c
@@ -1,11 +1,44 @@
-#include<stdio.h>
-#include<stdlib.h>
-#include<string.h>
+/*OOP
+ Literals:
+        Decimal  :  int i=50; base =10
+        Ocatal   :  int i=012; base =8;
+        Hexadecimal  :  int i=0x7f;  base =16
+        floating point  :  float x=2.5;
+        character   :   char "Hello";
 
-int main()
-{
-    char name[10] = "Hit";
-char *ptr = name;
-printf("%c", *ptr+3);
-return 0;
-}
+        escape sequences  :  \n \b \f \r \t \v \0
+
+ Constant:
+        const int i=5;
+
+ Datatypes:  primary & derived
+           
+        int short and long
+        float  long
+        double
+        char
+        wchar_t
+        bool
+        void
+
+ Type modifiers:
+        signed    
+        unsigned  :  0 to maxrange of datatype
+
+ short : -32768 to 32767
+
+
+ I/O
+
+    cout<< print
+    cin>> scan
+
+ operators:
+    arthimatic
+    logical
+    relational
+    bitwise
+    condition(assignment)
+
+    */
+

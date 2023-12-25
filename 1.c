141eb010324b41b3ea48122469d9e34b63aae7ca
Prince kumar <122357150+itzprincephutela@users.noreply.github.com>
2023-12-25 22:31:22 +0530

    Add files via upload

--- 1.c
+++ 1.c
@@ -1,30 +1,14 @@
-#include <stdio.h>
-#include <string.h>
-
-struct dob{
-    int date;
-    char month[20];
-    int year;
+#include<stdio.h>
+struct std{
+    char a;
+    char b;
+    int c:2;
+    char d;
 };
+struct std s1;
 
-struct student {
-    int roll;
-    char name[20];
-    int marks;
-    struct dob d1;
-};
-int main(){
-    struct student s1;
-    printf("Enter the name of student \n");
-    scanf("%s",s1.name);
-    printf("Enter the roll number of student \n");
-    scanf("%d",&s1.roll);
-    printf("Enter the marks of student \n");
-    scanf("%d",&s1.marks);
-    printf("Enter DOB of %s\n",s1.name);
-    scanf("%d%s%d",&s1.d1.date,s1.d1.month,&s1.d1.year);
-    printf("the id of %s is %d\n",s1.name,s1.roll);
-    printf("the marks of %s is %d\n",s1.name,s1.marks);
-    printf("Date of birth is: %d-%s-%d",s1.d1.date,s1.d1.month,s1.d1.year);
+int main()
+{
+    printf("%lu\n",sizeof(s1));
     return 0;
 }
